ff6196276977cb7ff839edc329e5227f2b948002
Brent Windham <pz9580mc@my.century.edu>
2018-10-13 16:02:56 -0500

    Added DealerShip class and .gitignore

---
+++ src/DealerShip.java
@@ -0,0 +1,21 @@
+
+public class DealerShip {
+
+	private String name;
+	
+	public DealerShip(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return "DealerShip [name=" + name + "]";
+	}
+}
